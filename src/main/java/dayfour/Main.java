30190e8bf1d1282612441d974c96e9688c5331fe
Mohamed Wagieh <mohamedwagimohamedwageih2891@gmail.com>
2026-06-08 00:16:31 +0300

    day 4

--- src/main/java/dayfour/Main.java
+++ src/main/java/dayfour/Main.java
@@ -6,9 +6,24 @@ public class Main {
         /*Sum s = new Sum();
         s.sum2num(10,20);
         hamada();*/
-        rectangle rectangle = new rectangle();  // now im calling method from class
+        /*rectangle rectangle = new rectangle();  // now im calling method from class
         // i have a cope of rectangle class that had a rectangle method so i have the ability to view what insid this class
-        double x = rectangle.calcArea();
+        double x = rectangle.calcArea();*/
+        rectangle   r1 = new rectangle();
+        r1.setLength(10);
+        r1.setWidth(20);
+        double total = r1.calcArea();
+        System.out.println("The area of the rectangle is: " + total);
+
+        //لو عايز استخد ال method في نفس ال class
+        rectangle r2 = new rectangle();
+        r2.setLength(5000);
+        r2.setWidth(20);
+        double r2total =r2.calcArea();
+        System.out.println("The area of the rectangle is: " + r2total);
+        public double bothtotal = total + r2total;
+        System.out.println("The area of both rectangle is: " + bothtotal);
+
     }
     public static void hamada(){
         System.out.println("hello hamada");
--- src/main/java/dayfour/rectangle.java
+++ src/main/java/dayfour/rectangle.java
@@ -4,17 +4,41 @@ import java.util.Scanner;
 
 public class rectangle {
         Scanner input = new Scanner(System.in);
-        public double length;
-        public double width;
+        private double length;
+        private double width;
+
+        public void setLength(double length) {
+            if (length > 0)
+                this.length = length;
+            else
+                System.out.println("the length is -ve");
+        }
+        public double getlength() {
+            return length;
+        }
+        public void setWidth(double width) {
+            if (width > 0)
+                this.width = width;
+            else
+                System.out.println("the width is -ve");
+        }
+        public double getwidth() {
+            return length;
+        }
+        public double calcArea () {
+            return length * width;
+            //return getlength() * getwidth();
+        }
 
 
-        public double calcArea(){
+
+        /*public double calcArea(){
             System.out.println("Please enter the length " );
             double length = input.nextDouble();
             System.out.println("Please enter the width ");
             double width = input.nextDouble();
             double area = length * width;
             System.out.println("The area of the rectangle is " + area);
-            return area; //its function to store value of the area
-        }
+            return area; //its function to store value of the area*/
+
 }
